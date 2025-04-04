2550b3c794f08f33fb1df07b696fe574daa6a6a5
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-04-04 20:00:09 +0100

    feat: initialize frontend structure with Next.js and Tailwind CSS setup

---
+++ frontend/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LoginForm } from "@/components/login-form";
+
+const SignIn = () => {
+  return (
+    <div className="w-full max-w-sm">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ frontend/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import { SignUpForm } from "@/components/sign-up";
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="w-full max-w-sm">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignUp;
