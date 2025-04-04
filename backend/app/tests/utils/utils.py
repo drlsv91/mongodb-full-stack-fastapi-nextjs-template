1fd0dd6982485c102c80a7d1a57907af5db0a2da
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-04-04 16:13:07 +0100

    refactor: remove unnecessary blank lines in utils.py

--- backend/app/tests/utils/utils.py
+++ backend/app/tests/utils/utils.py
@@ -1,8 +1,6 @@
 import random
 import string
-
 from fastapi.testclient import TestClient
-
 from app.core.config import settings
 
 
@@ -19,7 +17,9 @@ def get_superuser_token_headers(client: TestClient) -> dict[str, str]:
         "username": settings.FIRST_SUPERUSER,
         "password": settings.FIRST_SUPERUSER_PASSWORD,
     }
+
     r = client.post(f"{settings.API_V1_STR}/login/access-token", data=login_data)
+
     tokens = r.json()
     a_token = tokens["access_token"]
     headers = {"Authorization": f"Bearer {a_token}"}
